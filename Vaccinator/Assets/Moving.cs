62cc5d620bda74f5d0a9d15bd66a84bad9db6c03
MaherJ <Maherj.jaber23@gmail.com>
2020-02-28 09:19:42 +0100

    Moving enemy on the map

---
+++ Vaccinator/Assets/Moving.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moving : MonoBehaviour
+{
+    public float speed;
+    public Waypoints wayPoints;
+
+    private int waypointnumber;
+   
+    void Start()
+    {
+        wayPoints = GameObject.FindGameObjectWithTag("Waypoints").GetComponent<Waypoints>();
+    }
+
+    
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, wayPoints.waypoints[waypointnumber].position, speed * Time.deltaTime);
+
+        Vector3 direction = wayPoints.waypoints[waypointnumber].position - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+        if (Vector2.Distance(transform.position, wayPoints.waypoints[waypointnumber].position) < 0.1f)
+        {
+            if (waypointnumber < wayPoints.waypoints.Length - 1)
+            {
+                waypointnumber++;
+            }
+           
+
+        }
+    }
+}
