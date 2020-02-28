c614474fca10c0f172ef426918eed647da9e8fe0
Maherj <Maherj.jaber23@gmail.com>
2020-02-28 14:55:50 +0100

    Drag and drop, shooting and rotating implemented to the turret

---
+++ Vaccinator/Assets/Scripts/DragDrop.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragDrop : MonoBehaviour
+{
+    private bool Selected;
+
+    void Update()
+    {
+        if (Selected)
+        {
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            transform.Translate(mousePosition);
+        }
+        //if (Selected == true)
+        //{
+        //    Vector2 mPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //    transform.position = new Vector2(mPosition.x, mPosition.y);
+        //}
+        
+        //if(Input.GetMouseButtonUp(0))
+        //{
+        //    Selected = false;
+        //}
+    }
+
+    //void OnMouseOver()
+    //{
+    //    if (Input.GetMouseButtonDown(0))
+    //    {
+    //        Selected = true;
+    //    }
+    //}
+    public void OnMouseDown ()
+    {
+        Selected = true;
+        
+    }
+     
+    public void OnMouseUp ()
+    {
+        Selected = false;
+    }
+ 
+}
