f7482f606bf590ca1348cb63e09741a0eaed473c
Jesper Pettersson <TheSpiten@users.noreply.github.com>
2020-03-05 14:52:55 +0100

    Added Enemytracking and range to turret
    Need to add negative number tracking to turret

--- Vaccinator/Assets/Script/Turret.cs
+++ Vaccinator/Assets/Script/Turret.cs
@@ -7,31 +7,85 @@ public class Turret : MonoBehaviour
     [Header("Attributes")]
 
     public float speed = 10f;
-    public Transform target;
+    private Transform target;
+    public float range = 15;
+
+    public string enemyTag = "Enemy";
+    public Transform Rotation;
+
     public float fireRate = 1f;
     private float fireCountdown = 0f;
 
     public GameObject bulletPrefab;
     public Transform firePoint;
-   
+
+    private void Start()
+    {
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
+    }
+
+    void UpdateTarget()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+        float shortestDistance = Mathf.Infinity;
+        //TODO: MAKE A SUBSTITUTE FOR THE MATH INFINITY THAT COVERS NEGATIVE NUMBERS
+        GameObject nearestEnemy = null;
+        foreach(GameObject enemy in enemies)
+        {
+            float distancetToEnemy = Vector2.Distance(transform.position, enemy.transform.position);
+            if (distancetToEnemy < shortestDistance)
+            {
+                shortestDistance = distancetToEnemy;
+                nearestEnemy = enemy;
+            }
+
+        }
+        if (nearestEnemy != null && shortestDistance <= range)
+        {
+            target = nearestEnemy.transform;
+        }
+        else
+        {
+            target = null;
+        }
+    }
     // Update is called once per frame
     private void Update()
     {
-        Vector2 direction = target.position - transform.position;
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, speed * Time.deltaTime);
-
-        if(fireCountdown <= 0f)
+        if(target == null)
         {
-            Shoot();
-            fireCountdown = 1f / fireRate;
+            return;
         }
-        fireCountdown -= Time.deltaTime;
+
+        Vector2 dir = target.position - transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(dir);
+        Vector2 rotation = lookRotation.eulerAngles;
+        Rotation.rotation = Quaternion.Euler(0f, 0f, rotation.x);
+
+        //Vector2 direction = target.position - transform.position;
+        //float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        //Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        //transform.rotation = Quaternion.Slerp(transform.rotation, rotation, speed * Time.deltaTime);
+
+        //if(fireCountdown <= 0f)
+        //{
+        //    Shoot();
+        //    fireCountdown = 1f / fireRate;
+        //}
+        //fireCountdown -= Time.deltaTime;
+    }
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
     }
     void Shoot()
     {
         Debug.Log("Shoot");
-        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        GameObject bulletGO = (GameObject)Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Bullet bullet = bulletGO.GetComponent<Bullet>();
+
+        if (bullet != null)
+            bullet.Seek(target);
     }
 }
